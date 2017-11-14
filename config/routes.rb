580dd88214ead2807c2a68f0eb1871fa6fa757b4
JJ Keeney <jjkeeney129285@gmail.com>
2017-11-14 11:53:33 -0700

    edit profile is almost working

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,14 @@
 Rails.application.routes.draw do
-
+  devise_for :users, path: '', path_names: {sign_in: 'login', sign_out: 'logout', sign_up: 'register', edit: "settings"}
   resources :comments
   resources :users_connect, only: [:show]
   resources :posts
-  devise_for :users
+  # get "/settings", to: "devise/registrations#edit"
+   # devise_for :users #, controllers: { registrations: 'registrations' }
 
 
   get 'meetings', to: 'pages#meetings'
   get 'area', to: 'pages#area'
-  get 'settings', to: 'pages#settings'
-
   get 'profile', to: 'pages#profile'
   get 'landing_page', to: 'pages#landing_page'
   get 'login', to: 'pages#login'
