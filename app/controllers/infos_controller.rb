3911ba83af4387dbfaa5b49169150cf341b33688
JJ Keeney <jjkeeney129285@gmail.com>
2017-11-22 15:34:49 -0700

    almost finished with info controller

--- app/controllers/infos_controller.rb
+++ app/controllers/infos_controller.rb
@@ -1,19 +1,54 @@
 class InfosController < ApplicationController
+  before_action :authenticate_user! 
+  before_action :set_info, only: [ :edit, :update]
+  
+
   def new
     if current_user.infos.count == 1
-      redirect_to edit_infos_path
+      redirect_to edit_post_path
+    else  
+      @info = Info.new
     end  
   end
 
   def create
-    
+    @info = Info.new(info_params)
+    @info.user_id = current_user.id
+    if @info.save
+      redirect_to posts_path, notice: 'Your post was created successfully'
+     else
+     render :new  
+   end
   end
 
-  def update
-    
+  def show
+
   end
 
   def edit
-    
   end
+
+  def update
+    if @info.update(info_params)
+      redirect_to @info, notice: 'Your post was updated successfully'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @info.delete
+    redirect_to posts_path, notice: "Your post was deleted"
+  end
+
+  private
+
+  def set_info
+    @post = Info.find(params[:id])
+  end
+
+  def info_params
+    params.require(:info).permit(:area, :first_name, :last_name, )
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable,  :trackable, :validatable
   has_many :posts, dependent: :destroy
-  has_many :infos, dependent: :destroy      
+  has_many :infos, dependent: :destroy
+
+       
 end
