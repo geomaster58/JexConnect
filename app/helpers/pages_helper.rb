15050024683a8685e7c821b042a1e0161bae81f6
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-07 12:52:53 -0700

    finished picture upload to aws

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+  def image_generator(height:, width:)
+    "http://placehold.it/#{height}x#{width}"
+  end
+  
+  def info_img img
+  
+    if img.model.picture?
+      img
+    else
+      image_generator(height: '600', width: '400' )
+     end 
+  end
 end
