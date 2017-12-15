c7b6536b5e109d1309ded381fb8f3cac6a7f439d
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-15 15:49:12 -0700

    bashing my head against the wall for connections....

--- app/controllers/users_connect_controller.rb
+++ app/controllers/users_connect_controller.rb
@@ -3,14 +3,9 @@ class UsersConnectController < ApplicationController
     @user = User.find(params[:id])
   end
 
-  def create
-    @connection = Connection.new(:user_id)
-    @connection.user_id = current_user.id
-    if @connection.valid 
-      redirect_to connections_path, notice: 'You connected successfully.'
-    else
-      render :show
-    end
+  def follow
+    User.follow(2)
+    redirect_to root_path
   end
 
   
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :comments
-  resources :users_connect, only: [:show, :new, :create]
+  resources :users_connect, only: [:show]
   resources :posts
   resources :infos
   # get "/settings", to: "devise/registrations#edit"
-
+  get "follow", to: "users_connect#follow"
   get 'meetings', to: 'pages#meetings'
   get 'area', to: 'pages#area'
   get 'profile', to: 'pages#profile'
