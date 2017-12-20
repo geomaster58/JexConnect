0d3f35aaeeba02ef417e0596c8ee02247c3aaa45
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-20 15:15:02 -0700

    rethining migrations and data models

--- app/controllers/users_connect_controller.rb
+++ app/controllers/users_connect_controller.rb
@@ -2,13 +2,5 @@ class UsersConnectController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def follow
-    User.follow(2)
-    redirect_to root_path
-  end
-
-  
-
   
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171215213443) do
+ActiveRecord::Schema.define(version: 20171128222251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,16 +23,6 @@ ActiveRecord::Schema.define(version: 20171215213443) do
     t.datetime "updated_at", null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
     t.index ["user_id"], name: "index_comments_on_user_id"
-  end
-
-  create_table "connections", force: :cascade do |t|
-    t.integer "follower_id"
-    t.integer "followed_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["followed_id"], name: "index_connections_on_followed_id"
-    t.index ["follower_id", "followed_id"], name: "index_connections_on_follower_id_and_followed_id", unique: true
-    t.index ["follower_id"], name: "index_connections_on_follower_id"
   end
 
   create_table "friendly_id_slugs", force: :cascade do |t|
