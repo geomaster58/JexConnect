61ad38082fb2cf53345e739d25addbf2a27d4e5b
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-20 15:16:42 -0700

    deleted model for connections. Rethining

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,26 +5,7 @@ class User < ApplicationRecord
          :recoverable,  :trackable, :validatable
   has_many :posts, dependent: :destroy
   has_many :infos, dependent: :destroy
-  has_many :active_connections, class_name:  "Connection",
-                                  foreign_key: "follower_id",
-                                  dependent:   :destroy
-  has_many :passive_relationships, class_name:  "Relationship",
-                                   foreign_key: "followed_id",
-                                   dependent:   :destroy
-  has_many :following, through: :active_connections, source: :followed
-  has_many :followers, through: :passive_relationships, source: :follower
-
-  def follow(other_user)
-    following << other_user
-  end
-
-  def unfollow(other_user)
-    following.delete(other_user)
-  end
-
-  def following?(other_user)
-    following.include?(other_user)
-  end
+  
 
        
 end
