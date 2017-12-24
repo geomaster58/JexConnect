3bd40d4f358ad7feac13b9e87b46f4c9f0395776
JJ Keeney <jjkeeney129285@gmail.com>
2017-12-23 19:37:20 -0700

    initial tool for grabbing html from just for today

--- lib/jft_tool.rb
+++ lib/jft_tool.rb
@@ -1,5 +1,13 @@
-module Jft
-  def grab_html
-    
+module JftTool
+  require 'open-uri'
+  require 'openssl'
+
+  def self.grab_html
+    doc = Nokogiri::HTML(open('http://jftna.org/jft/', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
+    text = doc.css("table")
+    date = text.css('tr')[0].css('td')[0].css("h2")[0].text
+    print date
+    data = {date: date}
+    print data
   end
 end
