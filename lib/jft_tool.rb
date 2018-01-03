154c0a56bd1fb92817d761b8c3b0a58b8139e891
JJ Keeney <jjkeeney129285@gmail.com>
2018-01-03 02:17:27 -0700

    added haeding to scraper

--- lib/jft_tool.rb
+++ lib/jft_tool.rb
@@ -6,8 +6,9 @@ module JftTool
     doc = Nokogiri::HTML(open('http://jftna.org/jft/', :ssl_verify_mode => OpenSSL::SSL::VERIFY_NONE))
     text = doc.css("table")
     date = text.css('tr')[0].css('td')[0].css("h2")[0].text
+    heading = text.css("tr")[1].css("td")[0].css("h1")[0].text
     print date
-    data = {date: date}
-    print data
+    print heading
+    data = {date: date, heading: heading}
   end
 end
